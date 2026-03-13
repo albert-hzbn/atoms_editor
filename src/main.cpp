c00a8b85659c6ba516a2477cbac152906b6ec98f
albert-hzbn <albert.hzbn@gmail.com>
2026-03-13 10:21:04 +0100

    file browser and menu bar  added

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@
 #include "Camera.h"
 #include "SphereMesh.h"
 #include "Shader.h"
-#include "Controls.h"
 #include "ShadowMap.h"
 #include "StructureLoader.h"
 
@@ -16,7 +15,12 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <dirent.h>
+#include <sys/stat.h>
+#include <algorithm>
+#include <cstdlib>
 #include <vector>
+#include <cstring>
 #include <iostream>
 
 int main()
@@ -74,46 +78,101 @@ int main()
 
     Structure structure = loadStructure(filename);
 
+    bool showAbout = false;
+    bool openStructurePopup = false;
+
+    std::string openDir = ".";
+    auto pos = filename.find_last_of("/\\");
+    if (pos != std::string::npos)
+        openDir = filename.substr(0, pos);
+
+    std::vector<std::string> dirHistory;
+    int historyIndex = -1;
+
+    auto pushHistory = [&](const std::string& dir) {
+        if (historyIndex + 1 < (int)dirHistory.size())
+            dirHistory.erase(dirHistory.begin() + historyIndex + 1, dirHistory.end());
+        dirHistory.push_back(dir);
+        historyIndex = (int)dirHistory.size() - 1;
+    };
+
+    // Start history with initial folder
+    pushHistory(openDir);
+
+    // Drive roots (Unix style) - include home + common mount points
+    std::vector<std::string> driveRoots;
+    driveRoots.push_back("/");
+    if (const char* home = std::getenv("HOME"))
+        driveRoots.push_back(home);
+    driveRoots.push_back("/mnt");
+    driveRoots.push_back("/media");
+
+    // File browser filters
+    const std::vector<std::string> allowedExtensions = {".cif", ".mol", ".pdb", ".xyz", ".sdf"};
+    auto toLower = [&](const std::string& s) {
+        std::string out = s;
+        for (auto& c : out)
+            c = (char)tolower(c);
+        return out;
+    };
+
+    char openFilename[1024] = "";
+    std::string initialOpenFilename = filename;
+    if (pos != std::string::npos)
+        initialOpenFilename = filename.substr(pos + 1);
+
+    strncpy(openFilename, initialOpenFilename.c_str(), sizeof(openFilename));
+    openFilename[sizeof(openFilename) - 1] = '\0';
+
     // ------------------------------------------------
     // Instance buffers
     // ------------------------------------------------
 
     std::vector<glm::vec3> positions;
     std::vector<glm::vec3> colors;
+    size_t atomCount = 0;
 
-    for(const auto& atom : structure.atoms)
-    {
-        positions.emplace_back(atom.x,atom.y,atom.z);
-        colors.emplace_back(atom.r,atom.g,atom.b);
-    }
-
-    GLuint instanceVBO,colorVBO;
+    GLuint instanceVBO, colorVBO;
 
     glBindVertexArray(sphere.vao);
 
-    glGenBuffers(1,&instanceVBO);
-    glBindBuffer(GL_ARRAY_BUFFER,instanceVBO);
+    glGenBuffers(1, &instanceVBO);
+    glGenBuffers(1, &colorVBO);
 
-    glBufferData(GL_ARRAY_BUFFER,
-                 positions.size()*sizeof(glm::vec3),
-                 positions.data(),
-                 GL_STATIC_DRAW);
+    auto updateBuffers = [&](const Structure& s) {
+        positions.clear();
+        colors.clear();
 
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,sizeof(glm::vec3),(void*)0);
-    glVertexAttribDivisor(1,1);
+        for (const auto& atom : s.atoms)
+        {
+            positions.emplace_back(atom.x, atom.y, atom.z);
+            colors.emplace_back(atom.r, atom.g, atom.b);
+        }
 
-    glGenBuffers(1,&colorVBO);
-    glBindBuffer(GL_ARRAY_BUFFER,colorVBO);
+        atomCount = s.atoms.size();
 
-    glBufferData(GL_ARRAY_BUFFER,
-                 colors.size()*sizeof(glm::vec3),
-                 colors.data(),
-                 GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+        glBufferData(GL_ARRAY_BUFFER,
+                     positions.size() * sizeof(glm::vec3),
+                     positions.data(),
+                     GL_STATIC_DRAW);
 
-    glEnableVertexAttribArray(2);
-    glVertexAttribPointer(2,3,GL_FLOAT,GL_FALSE,sizeof(glm::vec3),(void*)0);
-    glVertexAttribDivisor(2,1);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
+        glVertexAttribDivisor(1, 1);
+
+        glBindBuffer(GL_ARRAY_BUFFER, colorVBO);
+        glBufferData(GL_ARRAY_BUFFER,
+                     colors.size() * sizeof(glm::vec3),
+                     colors.data(),
+                     GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
+        glVertexAttribDivisor(2, 1);
+    };
+
+    updateBuffers(structure);
 
     // ------------------------------------------------
     // Shader
@@ -232,7 +291,213 @@ int main()
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        drawControls(camera);
+        // ----------------------
+        // Main menu bar
+        // ----------------------
+        if (ImGui::BeginMainMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
+            {
+                if (ImGui::MenuItem("Open..."))
+                    openStructurePopup = true;
+
+                if (ImGui::MenuItem("Quit"))
+                    glfwSetWindowShouldClose(window, true);
+
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Help"))
+            {
+                if (ImGui::MenuItem("About"))
+                    showAbout = true;
+
+                ImGui::EndMenu();
+            }
+
+            ImGui::EndMainMenuBar();
+        }
+
+        if (openStructurePopup)
+        {
+            ImGui::OpenPopup("Open Structure");
+            openStructurePopup = false;
+        }
+
+        if (ImGui::BeginPopupModal("Open Structure", NULL, ImGuiWindowFlags_AlwaysAutoResize))
+        {
+            ImGui::Text("Current folder: %s", openDir.c_str());
+            ImGui::SameLine();
+            if (ImGui::Button(".."))
+            {
+                auto pos = openDir.find_last_of("/\\");
+                if (pos != std::string::npos)
+                    openDir = openDir.substr(0, pos);
+                else
+                    openDir = ".";
+                pushHistory(openDir);
+            }
+
+            ImGui::SameLine();
+            if (ImGui::Button("Back") && historyIndex > 0)
+            {
+                historyIndex--;
+                openDir = dirHistory[historyIndex];
+            }
+            ImGui::SameLine();
+            if (ImGui::Button("Forward") && historyIndex + 1 < (int)dirHistory.size())
+            {
+                historyIndex++;
+                openDir = dirHistory[historyIndex];
+            }
+
+            ImGui::SameLine();
+            if (ImGui::Button("Root"))
+            {
+                openDir = "/";
+                pushHistory(openDir);
+            }
+            ImGui::SameLine();
+            if (ImGui::Button("Home") && !driveRoots.empty())
+            {
+                openDir = driveRoots[1];
+                pushHistory(openDir);
+            }
+
+            ImGui::Separator();
+
+            if (ImGui::BeginChild("##filebrowser", ImVec2(500, 300), true))
+            {
+                DIR* dir = opendir(openDir.c_str());
+                if (dir)
+                {
+                    struct dirent* de;
+                    std::vector<std::pair<std::string, bool>> entries;
+
+                    while ((de = readdir(dir)) != nullptr)
+                    {
+                        std::string name(de->d_name);
+                        if (name == "." || name == "..")
+                            continue;
+
+                        std::string fullPath = openDir + "/" + name;
+                        struct stat st;
+                        bool isDir = (stat(fullPath.c_str(), &st) == 0 && S_ISDIR(st.st_mode));
+
+                        if (!isDir)
+                        {
+                            std::string ext;
+                            auto dot = name.find_last_of('.');
+                            if (dot != std::string::npos)
+                                ext = toLower(name.substr(dot));
+
+                            bool keep = false;
+                            for (const auto& allowed : allowedExtensions)
+                            {
+                                if (ext == allowed)
+                                {
+                                    keep = true;
+                                    break;
+                                }
+                            }
+                            if (!keep)
+                                continue;
+                        }
+
+                        entries.emplace_back(name, isDir);
+                    }
+                    closedir(dir);
+
+                    // directories first
+                    std::sort(entries.begin(), entries.end(),
+                              [](const std::pair<std::string, bool>& a,
+                                 const std::pair<std::string, bool>& b) {
+                        if (a.second != b.second)
+                            return a.second > b.second;
+                        return a.first < b.first;
+                    });
+
+                    for (size_t i = 0; i < entries.size(); ++i)
+                    {
+                        const std::string& name = entries[i].first;
+                        bool isDir = entries[i].second;
+
+                        ImGui::PushID((int)i);
+                        if (isDir)
+                        {
+                            std::string label = std::string("[DIR] ") + name + "##" + name;
+                            if (ImGui::Selectable(label.c_str()))
+                            {
+                                if (openDir == ".")
+                                    openDir = name;
+                                else
+                                    openDir = openDir + "/" + name;
+                                pushHistory(openDir);
+                            }
+                        }
+                        else
+                        {
+                            std::string label = name + "##" + name;
+                            bool selected = (std::string(openFilename) == name);
+                            if (ImGui::Selectable(label.c_str(), selected))
+                            {
+                                strncpy(openFilename, name.c_str(), sizeof(openFilename));
+                                openFilename[sizeof(openFilename) - 1] = '\0';
+                            }
+                        }
+                        ImGui::PopID();
+                    }
+                }
+                else
+                {
+                    ImGui::TextDisabled("Unable to open folder");
+                }
+
+                ImGui::EndChild();
+            }
+
+            ImGui::InputText("Filename", openFilename, sizeof(openFilename));
+
+            if (ImGui::Button("Load"))
+            {
+                std::string fullPath = openDir + "/" + openFilename;
+                Structure newStructure = loadStructure(fullPath);
+                if (!newStructure.atoms.empty())
+                {
+                    structure = std::move(newStructure);
+                    updateBuffers(structure);
+                }
+                ImGui::CloseCurrentPopup();
+            }
+            ImGui::SameLine();
+            if (ImGui::Button("Cancel"))
+            {
+                ImGui::CloseCurrentPopup();
+            }
+
+            ImGui::EndPopup();
+        }
+
+        if (showAbout)
+        {
+            ImGui::OpenPopup("About");
+            showAbout = false;
+        }
+
+        if (ImGui::BeginPopupModal("About", NULL, ImGuiWindowFlags_AlwaysAutoResize))
+        {
+            ImGui::Text("Atoms Editor");
+            ImGui::Text("Open-source molecular visualization demo.");
+            ImGui::Separator();
+            ImGui::TextWrapped("Controls:");
+            ImGui::BulletText("Left drag: rotate camera");
+            ImGui::BulletText("Scroll: zoom");
+            ImGui::TextWrapped("Use File -> Open to browse and load structure files.");
+            ImGui::Separator();
+            if (ImGui::Button("OK"))
+                ImGui::CloseCurrentPopup();
+            ImGui::EndPopup();
+        }
 
         ImGui::Render();
 
